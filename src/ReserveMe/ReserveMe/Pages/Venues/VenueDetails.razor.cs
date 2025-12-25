94f4a8026d35b785669b8de3f8d4565d7efca545
Tencho <tencho1011@gmail.com>
2025-12-25 17:43:47 +0200

    add page structure

---
+++ src/ReserveMe/ReserveMe/Pages/Venues/VenueDetails.razor.cs
@@ -0,0 +1,20 @@
+﻿namespace ReserveMe.Pages.Venues
+{
+	using Microsoft.AspNetCore.Components;
+	using Shared.Dtos.Venues;
+	using Shared.Services.Venues;
+
+	public partial class VenueDetails
+	{
+		[Parameter] public int VenueId { get; set; }
+
+		[Inject] private IVenuesService _venuesService { get; set; } = null!;
+
+		private VenueSearchDto venue { get; set; }
+
+		protected override async Task OnInitializedAsync()
+		{
+			this.venue = await this._venuesService.GetVenueById(VenueId);
+		}
+	}
+}
